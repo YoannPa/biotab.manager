a296f07b621c0e729b81a35a375aafd033143a6f
Pageaud <yoann.pageaud@gmail.com>
2019-10-01 17:32:58 +0200

    New function get.TCGA.biotab(); function get_TCGAproj_biotab() name changed to get.ls.TCGA.biotab().

--- make_TCGAproj_biotab.R
+++ make_TCGAproj_biotab.R
@@ -4,19 +4,92 @@ lapply(Imports, library, character.only = T)
 
 ##FUNCTIONS
 
-##get_TCGAproj_biotab ##########################################################
+#' @description Function description.
+#' 
+#' @param proj.id A \code{character} specifying a TCGA project name.
+#' @param dl.dir  A \code{character} specifying the path where downloaded
+#'                clinical raw data should be saved. If NULL, a folder 'GDCdata'
+#'                is created in the working directory (Default: dl.dir = NULL).
+#' @param rm.data A \code{logical} specifying whether to remove the downloaded
+#'                data or not once the biotab retrieved.
+#' @value A \code{list} containing 6 elements:
+#'        - biotab: a \code{data.frame} containing the project clinical data.
+#'        - description.table: a \code{data.frame} containing short description
+#'          of the biotab column names.
+#'        - CDE_ID.table: a \code{data.frame} containing the Clinical Data
+#'          Element IDs for each column of the biotab.
+#'        - warnings.query: a \code{list} of the possible warnings related to
+#'          GDCquery().
+#'        - warnings.DL: a \code{list} of the possible warnings related to
+#'          GDCdownload().
+#'        - warnings.preps: a \code{list} of the possible warnings related to
+#'          GDCprepare().
+#' @author Yoann Pageaud.
+#' @export
+#' @examples
+#' @references
 
-get_TCGAproj_biotab<-function(project.id,clin.data.dir){
+get.TCGA.biotab<-function(proj.id, dl.dir=NULL, rm.data=FALSE){
+  if(grepl(pattern = "^TCGA-*", x = proj.id)){
+    cat(paste0(proj.id,"\n"))
+    #Create GDC query
+    warn.query <- tryCatch.W.E(
+      query <- GDCquery(project = proj.id, data.category = "Clinical",
+                        data.type = "Clinical data", legacy = TRUE,
+                        file.type = "txt"))$warning
+    #Download clinical data
+    if(!is.null(dl.dir)){ main_wd<-getwd() ; setwd(dl.dir) }
+    warn.DL <-tryCatch.W.E(GDCdownload(query))$warning
+    #Get clinical biotab 
+    warn.prep <- tryCatch.W.E(Prep.query <- GDCprepare(query))$warning
+    if(!is.null(dl.dir)){
+      if(rm.data){ unlink("GDCdata/", recursive = TRUE) }
+      setwd(main_wd)
+    } else { if(rm.data){ unlink("GDCdata/", recursive = TRUE) } }
+    #Make column description table
+    description.tbl<-data.frame(
+      "column.names" = colnames(Prep.query[[1]]),
+      "column.descriptions" = c(as.matrix(Prep.query[[1]][1,])))
+    description.tbl<-description.tbl[sort(description.tbl$column.names),]
+    #Make clinical data element table
+    CDE_ID.tbl<-data.frame(
+      "column.names" = colnames(Prep.query[[1]]),
+      "clinical.data.element.ID" = c(as.matrix(Prep.query[[1]][2,]))
+    )
+    CDE_ID.tbl<-CDE_ID.tbl[sort(CDE_ID.tbl$column.names),]
+    #Store clin.biotab
+    biotab<-as.data.frame(Prep.query[[1]][-c(1,2),])
+  } else { stop("Unknown TCGA project.")}
+  return(list("biotab" = biotab, "description.table" = description.tbl,
+              "CDE_ID.table" = CDE_ID.tbl, "warnings.query" = warn.query,
+              "warnings.DL" = warn.DL, "warnings.preps" = warn.prep))
+}
+
+#' @description Returns a list of biotabs, one biotab per TCGA project.
+#' 
+#' @param proj.ids A \code{character} vector containing TCGA projects names.
+#' @param dl.dir   A \code{character} specifying the path where downloaded
+#'                 clinical raw data should be saved.
+#' @value A \code{type} object returned description.
+#' @author Yoann Pageaud.
+#' @export
+#' @examples
+#' @references
+
+#TODO: use get.TCGA.biotab in the function.
+#TODO: replace for loop by lapply().
+get.ls.TCGA.biotab<-function(proj.ids,dl.dir){ 
   #Keep only TCGA projects
-  processable.proj<-project.id[grepl("^TCGA-*",project.id)]
-  
+  processable.proj<-proj.ids[grepl("^TCGA-*",proj.ids)]
   #Display projects that are not from TCGA
-  unprocessable.proj<-project.id[!(project.id %in% processable.proj)]
-  cat(paste(c("WARNING! Cannot process the following projects:",
-              unprocessable.proj,"\n")))
+  if(length(proj.ids[!(proj.ids %in% processable.proj)]) != 0){
+    unprocessable.proj<-proj.ids[!(proj.ids %in% processable.proj)]
+    cat(paste(c("WARNING! Cannot process the following projects:",
+                unprocessable.proj,"\n")))
+  } else { unprocessable.proj<-NULL }
   #Change working dir
   main_wd<-getwd()
-  setwd(clin.data.dir)
+  setwd(dl.dir)
   #Set lists
   list.biotab<-list()
   list.warn.query<-list()
@@ -49,7 +122,6 @@ get_TCGAproj_biotab<-function(project.id,clin.data.dir){
   return(res)
 }
 
-
 ##save.list.RDS ################################################################
 
 save.list.RDS<-function(list,dir.save){
@@ -63,7 +135,6 @@ save.list.RDS<-function(list,dir.save){
                                                                 ".RDS")))})
 }
 
-
 ##save.unproc.proj #############################################################
 
 save.unproc.proj<-function(biotab.res,dir.save){
@@ -73,7 +144,6 @@ save.unproc.proj<-function(biotab.res,dir.save){
   write.res<-lapply(biotab.res[["unprocessed.projects"]], write,
                     file.path(dir.save,"Unprocessed_Projects.txt"),append = T)
 }
-
 
 ##save.warnings ################################################################
 
